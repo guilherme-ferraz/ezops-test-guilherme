7839597650022b753471d891488b3522754c7f51
Guilherme Ferraz <guilhermeaugusto@rubeus.com.br>
2021-07-05 21:23:42 -0300

    script/deploy.sh

--- scripts/deploy_to_aws.sh
+++ scripts/deploy_to_aws.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+echo "${{ secrets.AWS_SSH_PEM }}" > cert.pem
+chmod 400 cert.pem
+mkdir -p ~/.ssh/
+chmod 700 ~/.ssh/
+ssh-keyscan -H "${{ secrets.AWS_KNOWN_HOSTS }}" >> ~/.ssh/known_hosts
 
-echo "ola mundo $(date)" >> deploy.log
+ssh -i 'cert.pem' ${{ secrets.AWS_USER }}@${{ secrets.AWS_HOST }} 'echo "ola" > test.remote && pwd'
