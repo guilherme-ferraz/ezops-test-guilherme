c82813ec98c172585fff4fb01ad4408104e1e0fe
Guilherme Ferraz <guilhermeaugusto@rubeus.com.br>
2021-07-05 22:21:45 -0300

    try workflow env inside scriptp

--- scripts/workflowAWS.sh
+++ scripts/workflowAWS.sh
@@ -24,7 +24,7 @@ done
 #config cert and ssh connection
 echo "$cert" > cert.pem && chmod 400 cert.pem
 echo "user: $user"
-echo "host: $host"
+echo "host: $AWS_HOST"
 echo "cert: $cert"
 mkdir -p ~/.ssh/ && chmod 700 ~/.ssh/
 ssh-keyscan -H "$host" >> ~/.ssh/known_hosts
