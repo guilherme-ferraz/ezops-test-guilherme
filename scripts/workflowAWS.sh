fc2a8058382294b134c93f258eaa5be9a132af2b
Guilherme Ferraz <guilhermeaugusto@rubeus.com.br>
2021-07-05 22:29:26 -0300

    print .env.production

--- scripts/workflowAWS.sh
+++ scripts/workflowAWS.sh
@@ -24,7 +24,8 @@ done
 #config cert and ssh connection
 echo "$cert" > cert.pem && chmod 400 cert.pem
 echo "user: $user"
-echo $host | base64 -d
+ls -lah
+cat .env.production
 echo "cert: $cert"
 mkdir -p ~/.ssh/ && chmod 700 ~/.ssh/
 ssh-keyscan -H "$host" >> ~/.ssh/known_hosts
