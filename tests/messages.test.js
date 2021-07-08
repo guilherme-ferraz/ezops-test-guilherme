d3f486cac2c4dce20ac9a9a695e52d87e7eeadf6
Guilherme Ferraz <guilhermeaugusto@rubeus.com.br>
2021-07-08 20:42:38 -0300

    add jtest

---
+++ tests/messages.test.js
@@ -0,0 +1,17 @@
+const request = require('supertest')
+const app = require('../server')
+
+const serverHost = 'localhost:3000';
+
+describe('message endpoints', () => {
+    it('get messages', async () => {
+        const res = await request('localhost:3000').get(`/`);
+        expect(res.statusCode).toEqual(200);
+    });
+
+    it('get messages especific user', async () => {
+        const postId = 'guilherme';
+        const res = await request('localhost:3000').get(`/messages/guilherme`);
+        expect(res.statusCode).toEqual(200);
+    });
+})
